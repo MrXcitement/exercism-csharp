2bd29e283efd018bdd0783de3dcfc0da2b79aea8
Mike Barker <mike@thebarkers.com>
2019-05-07 08:26:46 -0600

    two-fer - refactor TwoFer methods
    Based on mentor's suggestion, I replaced both TwoFer methods with one
    that had an optional argument defined.

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -1,6 +1,4 @@
 ﻿public static class TwoFer
 {
-    public static string Speak() => "One for you, one for me.";
-
-    public static string Speak(string name) => $"One for {name}, one for me.";
+    public static string Speak(string name = "you") => $"One for {name}, one for me.";
 }
